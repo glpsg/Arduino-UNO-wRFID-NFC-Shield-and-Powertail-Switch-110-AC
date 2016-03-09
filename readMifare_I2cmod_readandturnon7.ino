a0e0e35bf27c105fa64207c48526820e2b59417e
glpsg <scotttavitian@gmail.com>
2016-03-09 10:09:16 -0600

    Added files via upload

---
+++ readMifare_I2cmod_readandturnon7.ino
@@ -0,0 +1,124 @@
+/**************************************************************************/
+/*! 
+    @file     readMifare.pde
+    @author   Adafruit Industries
+	@license  BSD (see license.txt)
+
+    This code will wait for any ISO14443A card or tag, and
+    depending on the size of the UID will attempt to read from it.
+   
+    If the card has a 4-byte UID it is probably a Mifare
+    Classic card, and the following steps are taken:
+   
+    - Authenticate block 4 (the first block of Sector 1) using
+      the default KEYA of 0XFF 0XFF 0XFF 0XFF 0XFF 0XFF
+    - If authentication succeeds, we can then read any of the
+      4 blocks in that sector (though only block 4 is read here)
+	 
+    If the card has a 7-byte UID it is probably a Mifare
+    Ultralight card, and the 4 byte pages can be read directly.
+    Page 4 is read by default since this is the first 'general-
+    purpose' page on the tags.
+
+
+This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
+This library works with the Adafruit NFC breakout 
+  ----> https://www.adafruit.com/products/364
+ 
+Check out the links above for our tutorials and wiring diagrams 
+These chips use SPI or I2C to communicate.
+
+
+*/
+/**************************************************************************/
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_PN532.h>
+
+
+// If using the breakout or shield with I2C, define just the pins connected
+// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
+#define PN532_IRQ   (2)
+#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+
+
+// Use this line for a breakout or shield with an I2C connection:
+Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+// also change #define in Adafruit_PN532.cpp library file
+   #define Serial SerialUSB
+#endif
+
+boolean lightsOn = false; // The light is defined as off when the system first boots up
+
+void setup(void) {
+  #ifndef ESP8266
+    while (!Serial); 
+  #endif
+  Serial.begin(115200);
+  Serial.println("Hello!"); 
+
+  pinMode(12, OUTPUT); // This section sets up Pin 12 as the data line that will talk to the powerstrip
+
+  nfc.begin(); //This section starts the NFC on the RFID shield
+
+ 
+// This section uses the Serial Monitor to confirm that the RFID shield starts up and loads correctly
+
+  uint32_t versiondata = nfc.getFirmwareVersion();
+  if (! versiondata) {
+    Serial.print("Didn't find PN53x board");
+    while (1); // halt
+  }
+  // Got ok data, print it out!
+  Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
+  Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
+  Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  
+  // configure board to read RFID tags
+  nfc.SAMConfig();
+  
+  Serial.println("Waiting for an ISO14443A Card ..."); 
+  Serial.println("");
+}
+
+
+void loop(void) {
+  uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
+  // 'uid' will be populated with the UID, and uidLength will indicate
+  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+
+  uint32_t cardidentifier = 0;
+  
+  if (success) {
+     Serial.print("Card detected #");
+    // turn the four byte UID of a mifare classic into a single variable #
+    cardidentifier = uid[3];
+    cardidentifier <<= 8; cardidentifier |= uid[2];
+    cardidentifier <<= 8; cardidentifier |= uid[1];
+    cardidentifier <<= 8; cardidentifier |= uid[0];
+    Serial.println(cardidentifier); //This section reads the card # and prints it in the serial monitor
+
+
+//This Section turns on power strip if the correct card # is read, an affirmative message is sent to the serial monitor
+    if (cardidentifier == 4229978145 && lightsOn == false) {
+      Serial.println("Hello, I will turn on now.");
+      digitalWrite(12, HIGH);
+      delay(1000);
+    }  
+    //This section turns the light off if it is already on upon seeing a specific card #
+    if (cardidentifier == 4229978145 && lightsOn == true) { 
+      Serial.println("Hello, I will turn off now.");
+      digitalWrite(12, LOW);
+      delay(1000);
+    }
+    lightsOn=!lightsOn; 
+   }
+  }
